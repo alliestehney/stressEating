38ab92a80d66af7883cb884c0a9c4219ae6adf89
alliestehney <alliestehney1@gmail.com>
2017-11-28 09:26:09 -0500

    Added indentation

--- src/components/DietaryPref.js
+++ src/components/DietaryPref.js
@@ -3,44 +3,43 @@ import { connect } from 'react-redux';
 
 
 class DietaryPref extends Component{
-constructor(props){
-  super(props);
-  this.state = {
- VeganSelected:false,
- VegatarianSelected:false,
- GlutenSelected:false,
- NutSelected:false,
- DairySelected:false,
- NoneSelected:false,
-
-  };
-}
-render(){
-  return(
-    <form className = "DietaryPref">
-      <h2>Dietary Preferences</h2>
-      <label className="Dietary__label">Dietary Preferences</label>
-      <p>
-    <label> <input type="checkbox" checked={this.state.selected}
-                               onChange={this.handleVeganChange.bind(this)} /> Vegan </label>
+    constructor(props){
+      super(props);
+      this.state = {
+         VeganSelected:false,
+         VegatarianSelected:false,
+         GlutenSelected:false,
+         NutSelected:false,
+         DairySelected:false,
+         NoneSelected:false
+      };
+    }
+    render(){
+      return(
+        <form className = "DietaryPref">
+          <h2>Dietary Preferences</h2>
+          <label className="Dietary__label">Dietary Preferences</label>
+          <p>
         <label> <input type="checkbox" checked={this.state.selected}
-                                   onChange={this.handleVegetarianChange.bind(this)}/> Vegatarian </label>
-        <label>   <input type="checkbox" checked={this.state.selected}
-                                     onChange={this.handleGlutenFreeChange.bind(this)}/> Gluten-Free </label>
-        <label> <input type="checkbox" checked={this.state.selected}
-                                     onChange={this.handleNutFreeChange.bind(this)} /> Nut-Free </label>
+                                   onChange={this.handleVeganChange.bind(this)} /> Vegan </label>
             <label> <input type="checkbox" checked={this.state.selected}
-                                       onChange={this.handleDairyFreeChange.bind(this)}/> Dairy-Free </label>
+                                       onChange={this.handleVegetarianChange.bind(this)}/> Vegatarian </label>
+            <label>   <input type="checkbox" checked={this.state.selected}
+                                         onChange={this.handleGlutenFreeChange.bind(this)}/> Gluten-Free </label>
             <label> <input type="checkbox" checked={this.state.selected}
-                                         onChange={this.handleNoneChange.bind(this)}/> None </label>
+                                         onChange={this.handleNutFreeChange.bind(this)} /> Nut-Free </label>
+                <label> <input type="checkbox" checked={this.state.selected}
+                                           onChange={this.handleDairyFreeChange.bind(this)}/> Dairy-Free </label>
+                <label> <input type="checkbox" checked={this.state.selected}
+                                             onChange={this.handleNoneChange.bind(this)}/> None </label>
 
-            </p>
+                </p>
 
-      <button type="submit"> Submit</button>
+          <button type="submit"> Submit</button>
 
-    </ form>
-  );
-  }
+        </ form>
+      );
+      }
 
   handleVeganChange(event) {
         this.setState({
@@ -73,4 +72,5 @@ render(){
         });
     }
 }
+
 export default DietaryPref;
