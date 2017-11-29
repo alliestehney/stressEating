ec28368573026d53efe1c561401c9ea90733eeaa
alliestehney <alliestehney1@gmail.com>
2017-11-29 16:43:14 -0500

    Added styling changes

--- src/components/StressLevel.js
+++ src/components/StressLevel.js
@@ -17,9 +17,9 @@ class StressLevel extends Component {
           <div className="StressForm">
             <h1>Welcome, how do you feel?</h1>
             <ul className="StressIcon" >
-              <li>  <button className="StressLevelIcon" type="button" value="low" onClick={this.handleClick.bind(this)}>😎</button> </li>
-              <li> <button className="StressLevelIcon" type="button" value="medium" onClick={this.handleClick.bind(this)} >😨</button> </li>
-              <li>  <button className="StressLevelIcon" type="button" value="high" onClick={this.handleClick.bind(this)}>😫</button> </li>
+              <li>  <button className="StressLevelIcon" type="button" value="low" onClick={this.handleClick.bind(this)}><img className="Cool" src="cool.png" alt="not stressed" /></button> </li>
+              <li> <button className="StressLevelIcon" type="button" value="medium" onClick={this.handleClick.bind(this)} ><img className="Cool" src="freakedout.png" alt="not stressed" /></button> </li>
+              <li>  <button className="StressLevelIcon" type="button" value="high" onClick={this.handleClick.bind(this)}><img className="Cool" src="nervous.png" alt="not stressed" /></button> </li>
             </ul>
           </div>
 
