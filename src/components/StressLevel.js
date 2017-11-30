043ed34e8723e006d9baf0e1d98becc5b9fb2854
alliestehney <alliestehney1@gmail.com>
2017-11-30 13:54:19 -0500

    Changed middle emoji icon

--- src/components/StressLevel.js
+++ src/components/StressLevel.js
@@ -18,7 +18,7 @@ class StressLevel extends Component {
             <h1 className="Welcome">Welcome, how do you feel?</h1>
             <ul className="StressIcon" >
               <li><button className="StressLevelIcon" type="button" value="low" onClick={this.handleClick.bind(this)}><img className="Cool" src="cool.png" alt="not stressed" /></button> </li>
-              <li><button className="StressLevelIcon" type="button" value="medium" onClick={this.handleClick.bind(this)}><img className="Cool" src="freakedout.png" alt="not stressed" /></button></li>
+              <li><button className="StressLevelIcon" type="button" value="medium" onClick={this.handleClick.bind(this)}><img className="Cool" src="confused.png" alt="not stressed" /></button></li>
               <li><button className="StressLevelIcon" type="button" value="high" onClick={this.handleClick.bind(this)}><img className="Cool" src="nervous.png" alt="not stressed" /></button> </li>
             </ul>
           </div>
