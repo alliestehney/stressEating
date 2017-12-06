3e15934c4714ab938b6e645d107aa9d099445d3f
alliestehney <alliestehney1@gmail.com>
2017-12-05 20:50:31 -0500

    Organized switch cases

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -18,54 +18,50 @@ export default function(state=INITIAL_STATE, action) {
 			 return Object.assign({}, state, {
           cookorbuy: action.cookorbuy,
 				  currentComponent: "dietary_restrictions"
-		});
-        case "REQUEST_RECIPES":
-           return Object.assign({}, state, {
+		    });
+    case "REQUEST_RECIPES":
+        return Object.assign({}, state, {
         });
-        case "RECEIVE_RECIPES":
-            console.log("Test");
-            return Object.assign({}, state, {
-
-                recipes: action.recipes,
-                currentComponent: "recipe_list"
+    case "RECEIVE_RECIPES":
+        console.log("Test");
+        return Object.assign({}, state, {
+            recipes: action.recipes,
+            currentComponent: "recipe_list"
         });
-        case "CREATE_LOADING_LIST":
-            // when this action is called we want to add 3 more items from recipes into loading array
-            console.log("Going into loading list action");
+    case "CREATE_LOADING_LIST":
+        // when this action is called we want to add 3 more items from recipes into loading array
+        console.log("Going into loading list action");
 
-            const index = state.index;
-            const newLoadingArray = state.recipes.slice(0, index);
-            console.log("NEW LOADING ARRAY: ", newLoadingArray);
-            console.log("RECIPE ARRAY: ", state.recipes);
+        const index = state.index;
+        const newLoadingArray = state.recipes.slice(0, index);
+        console.log("NEW LOADING ARRAY: ", newLoadingArray);
+        console.log("RECIPE ARRAY: ", state.recipes);
 
-            return Object.assign({}, state, {
-                 loading: newLoadingArray,
-                 index: index+3
-             });
-       case "SHOW_COOK_OR_BUY":
-            return Object.assign({}, state, {
-				          stressLevel: action.stressLevel,
-		              currentComponent: "cook_or_buy"
-              });
-        case "REQUEST_RESTAURANTS":
-           return Object.assign({}, state, {
+        return Object.assign({}, state, {
+            loading: newLoadingArray,
+            index: index+3
         });
-
-        case "RECEIVE_RESTAURANTS":
-            return Object.assign({}, state, {
-                restaurants: action.restaurants,
-                currentComponent: "restaurants"
+    case "SHOW_COOK_OR_BUY":
+        return Object.assign({}, state, {
+				    stressLevel: action.stressLevel,
+		        currentComponent: "cook_or_buy"
         });
-        case "SHOW_RESTAURANTS":
-           return Object.assign({}, state, {
+    case "REQUEST_RESTAURANTS":
+        return Object.assign({}, state, {
+        });
+    case "RECEIVE_RESTAURANTS":
+        return Object.assign({}, state, {
+            restaurants: action.restaurants,
+            currentComponent: "restaurants"
+        });
+    case "SHOW_RESTAURANTS":
+        return Object.assign({}, state, {
              cookorbuy: action.cookorbuy,
              currentComponent: "restaurants"
         });
-        case "SHOW_ERROR_PAGE":
-           return Object.assign({}, state, {
-
+    case "SHOW_ERROR_PAGE":
+        return Object.assign({}, state, {
         });
-
 		default:
 			return state;
 	}
